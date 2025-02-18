2ea78d7258450605bcfbb79d0dae813ff96e737b
Kriszti <csuka.tina@gmail.com>
2025-02-18 15:04:54 +0100

    homework

---
+++ arrays/src/homework_loops/First_loop.java
@@ -0,0 +1,21 @@
+package homework_loops;
+
+import java.util.Scanner;
+
+public class First_loop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+        int number = Integer.parseInt(scanner.nextLine());
+        System.out.print("Please enter a text: ");
+        String text = scanner.nextLine();
+        if (number < 0) {
+            System.out.println("The number is less than zero.");
+        } else {
+            for (int i = 0; i < number; i++) {
+                System.out.println(text);
+            }
+        }
+
+    }
+}
---
+++ arrays/src/homework_loops/FizzBuzz.java
@@ -0,0 +1,23 @@
+package homework_loops;
+
+import java.util.Scanner;
+
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int number = 0;
+        do {
+            System.out.print("Please enter a number: ");
+            number = Integer.parseInt(scanner.nextLine());
+            if (number <= 0) {
+                System.out.println("The number must be greater than 0.");
+            }
+        } while (number <= 0);
+        for (int i = 0; i < number; i++) {
+            String text = (i % 3 == 0 && i % 5 == 0) ? "FizzBuzz" : (i % 3 == 0) ? "Fizz" : (i % 5 == 0) ? "Buzz" : String.valueOf(i);
+            System.out.println(text);
+        }
+    }
+}
