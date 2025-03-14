95bf2a5810f0d2e832f24a30d261de2f258b1f58
Kriszti <csuka.tina@gmail.com>
2025-03-14 20:16:45 +0100

    Improvements and bug fixes

--- arrays/src/StrangePolynomialSum.java
+++ arrays/src/StrangePolynomialSum.java
@@ -9,119 +9,114 @@ public class StrangePolynomialSum {
         int number = Integer.parseInt(scanner.nextLine());
         scanner.close();
         System.out.println("First possible solution:");
-        countStrangePolynomialSum(number);
+        int strangeSum = countStrangePolynomialSum(number);
+        System.out.println(checkInputAndComputeSum(strangeSum));
         System.out.println();
         System.out.println("Second possible solution:");
-        countStrangePolynomialSum2(number);
+        int strangeSum2 = countStrangePolynomialSum2(number);
+        System.out.println(checkInputAndComputeSum(strangeSum2));
         System.out.println();
         System.out.println("Difficult strange task solution: ");
-        countDifficultStrangeSum(number);
+        int difficultStrangeSum = countDifficultStrangeSum(number);
+        System.out.println(checkInputAndComputeSum(difficultStrangeSum));
+
     }
 
     public static boolean isGreaterThanOne(int number) {
-        return number < 1;
+        return number > 1;
     }
 
-    public static void countDifficultStrangeSum(int number) {
+    public static String checkInputAndComputeSum(int number) {
+        String stringNumber = String.valueOf(number);
+        return isGreaterThanOne(number) ? stringNumber : "End of the application";
+
+    }
+
+    public static int countDifficultStrangeSum(int number) {
         int sum = 0;
-        if (isGreaterThanOne(number)) {
-            System.out.println("End of the application");
-        } else {
-            for (int i = 1; i <= number; i++) {
-                if (i % 2 == 0) {
-                    if (i % 3 == 1) {
-                        if (i % 5 == 0) {
-                            sum -= 2 * (i);
-                        } else {
-                            sum -= i;
-                        }
-                    } else if (i % 3 == 2) {
-                        if (i % 5 == 0) {
-                            sum -= 2 * (i * i);
-                        } else {
-                            sum -= i * i;
-                        }
-                    } else if (i % 3 == 0) {
-                        if (i % 5 == 0) {
-                            sum -= 2 * (i * i * i);
-                        } else {
-                            sum -= i * i * i;
-                        }
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 == 0) {
+                if (i % 3 == 1) {
+                    if (i % 5 == 0) {
+                        sum -= 2 * (i);
+                    } else {
+                        sum -= i;
                     }
-                } else {
-                    if (i % 3 == 1) {
-                        if (i % 5 == 0) {
-                            sum += 2 * (i);
-                        } else {
-                            sum += i;
-                        }
-                    } else if (i % 3 == 2) {
-                        if (i % 5 == 0) {
-                            sum += 2 * (i * i);
-                        } else {
-                            sum += i * i;
-                        }
-                    } else if (i % 3 == 0) {
-                        if (i % 5 == 0) {
-                            sum += 2 * (i * i * i);
-                        } else {
-                            sum += i * i * i;
-                        }
+                } else if (i % 3 == 2) {
+                    if (i % 5 == 0) {
+                        sum -= 2 * (i * i);
+                    } else {
+                        sum -= i * i;
+                    }
+                } else if (i % 3 == 0) {
+                    if (i % 5 == 0) {
+                        sum -= 2 * (i * i * i);
+                    } else {
+                        sum -= i * i * i;
+                    }
+                }
+            } else {
+                if (i % 3 == 1) {
+                    if (i % 5 == 0) {
+                        sum += 2 * (i);
+                    } else {
+                        sum += i;
+                    }
+                } else if (i % 3 == 2) {
+                    if (i % 5 == 0) {
+                        sum += 2 * (i * i);
+                    } else {
+                        sum += i * i;
+                    }
+                } else if (i % 3 == 0) {
+                    if (i % 5 == 0) {
+                        sum += 2 * (i * i * i);
+                    } else {
+                        sum += i * i * i;
                     }
                 }
             }
-            System.out.println(sum*number);
-
         }
-
+        return sum * number;
     }
 
-    public static void countStrangePolynomialSum(int number) {
-        if (isGreaterThanOne(number)) {
-            System.out.println("End of the application");
-        } else {
-            int sum = 0;
-            int counter = 1;
-            int counter2 = 2;
-            int counter3 = 3;
-            int counter4 = 4;
-            for (int i = 1; i <= number; i++) {
-                if (i == counter) {
-                    sum += (int) Math.pow(i, 1);
-                    counter += 4;
-                } else if (i == counter2) {
-                    sum += (int) Math.pow(i, 2);
-                    counter2 += 4;
-                } else if (i == counter3) {
-                    sum += (int) Math.pow(i, 3);
-                    counter3 += 4;
-                } else if (i == counter4) {
-                    sum += (int) Math.pow(i, 4);
-                    counter4 += 4;
-                }
+    public static int countStrangePolynomialSum(int number) {
+        int sum = 0;
+        int counter = 1;
+        int counter2 = 2;
+        int counter3 = 3;
+        for (int i = 1; i <= number; i++) {
+            if (i == counter) {
+                sum += i;
+                counter += 4;
+            } else if (i == counter2) {
+                sum += (int) Math.pow(i, 2);
+                counter2 += 4;
+            } else if (i == counter3) {
+                sum += (int) Math.pow(i, 3);
+                counter3 += 4;
+            } else {
+                sum += (int) Math.pow(i, 4);
+
             }
-            System.out.println(sum);
         }
+        return sum;
     }
 
-    public static void countStrangePolynomialSum2(int number) {
-        if (isGreaterThanOne(number)) {
-            System.out.println("End of the application");
-        } else {
-            int sum = 0;
-            for (int i = 1; i <= number; i++) {
-                if (i % 4 == 1)
-                    sum += (int) Math.pow(i, 1);
-                else if (i % 4 == 2) {
-                    sum += (int) Math.pow(i, 2);
-                } else if (i % 4 == 3) {
-                    sum += (int) Math.pow(i, 3);
-                } else if (i % 4 == 0) {
-                    sum += (int) Math.pow(i, 4);
-                }
-
+    public static int countStrangePolynomialSum2(int number) {
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            if (i % 4 == 1)
+                sum += i;
+            else if (i % 4 == 2) {
+                sum += (int) Math.pow(i, 2);
+            } else if (i % 4 == 3) {
+                sum += (int) Math.pow(i, 3);
+            } else {
+                sum += (int) Math.pow(i, 4);
             }
-            System.out.println(sum);
+
         }
+        return sum;
     }
 }
--- arrays/src/StrangePolynomialSumTests.java
+++ arrays/src/StrangePolynomialSumTests.java
@@ -3,24 +3,46 @@ import org.junit.jupiter.api.Test;
 
 public class StrangePolynomialSumTests {
     @Test
-    void returnTrueWhenNumberLessThan1() {
+    void returnFalseWhenNumberLessThan1() {
         int number = 0;
-        boolean result = StrangePolynomialSum.isGreaterThanOne(number);
-        Assertions.assertTrue(result);
-    }
-
-    @Test
-    void returnTrueWhenNumberIsMinus() {
-        int number = -4;
-        boolean result = StrangePolynomialSum.isGreaterThanOne(number);
-        Assertions.assertTrue(result);
-    }
-
-    @Test
-    void returnFalseWhenNumberGreaterThan1() {
-        int number = 2;
         boolean result = StrangePolynomialSum.isGreaterThanOne(number);
         Assertions.assertFalse(result);
     }
+
+    @Test
+    void returnFalseWhenNumberIsNegative() {
+        int number = -4;
+        boolean result = StrangePolynomialSum.isGreaterThanOne(number);
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    void returnTrueWhenNumberGreaterThan1() {
+        int number = 2;
+        boolean result = StrangePolynomialSum.isGreaterThanOne(number);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void returnValidStrangePolynomialSum() {
+        int number = 17;
+        int result = StrangePolynomialSum.countStrangePolynomialSum(number);
+        Assertions.assertEquals(96081, result);
+    }
+
+    @Test
+    void returnValidStrangePolynomialSum2() {
+        int number = 11;
+        int result = StrangePolynomialSum.countStrangePolynomialSum2(number);
+        Assertions.assertEquals(6208, result);
+    }
+
+    @Test
+    void returnValidDifficultStrangePolynomialSum() {
+        int number = 11;
+        int result = StrangePolynomialSum.countDifficultStrangeSum(number);
+        Assertions.assertEquals(6897, result);
+    }
+
 
 }
