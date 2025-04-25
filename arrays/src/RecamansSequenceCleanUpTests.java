9bc684cf582c26a8725dee8536f0cdd68c016fd8
Kriszti <csuka.tina@gmail.com>
2025-04-25 20:10:36 +0200

    Test correction

--- arrays/src/RecamansSequenceCleanUpTests.java
+++ arrays/src/RecamansSequenceCleanUpTests.java
@@ -10,7 +10,7 @@ public class RecamansSequenceCleanUpTests {
     @Test
     void returnRecamansNumbersRecursive() {
         List<Integer> expected = new ArrayList<>(Arrays.asList(0, 1, 3, 6, 2, 7, 13, 20, 12, 21, 11, 22, 10, 23, 9, 24));
-        List<Integer> result = RecamansSequence.fillListWithRecamansNumberRecursive(16, 0, 0, new ArrayList<>());
+        List<Integer> result = RecamansSequenceCleanUp.fillListWithRecamansNumberRecursive(16);
         assertEquals(expected, result);
     }
 }
